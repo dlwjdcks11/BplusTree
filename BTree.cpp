2bdda127d0f792357c7c9c915c6fb69624ee0159
dlwjdcks11 <dlwjdcks7356@gmail.com>
2021-06-04 02:48:28 +0900

    Modify file I/O part

--- BTree.cpp
+++ BTree.cpp
@@ -7,11 +7,10 @@
 #include <vector>
 using namespace std;
 
+const char* fileName;
+
 class BTree {
 public:
-	const char* fileName;
-	int blockSize;
-
 	BTree(const char*, int);
 	bool insert(int, int);
 	int* search(int);
@@ -22,21 +21,39 @@ public:
 BTree::BTree(const char* _fileName, int _blockSize) {
 	const int RootBID = 1;
 	const int Depth = 0;
-	this->fileName = _fileName;
-	this->blockSize = _blockSize;
 
 	ofstream makeBin;
-	makeBin.open(fileName, ios::out | ios::binary);
-	makeBin.write((char*)&blockSize, sizeof(blockSize));
+	makeBin.open(_fileName, ios::out | ios::binary);
+	makeBin.write((char*)&_blockSize, sizeof(_blockSize));
 	makeBin.write((char*)&RootBID, sizeof(RootBID));
 	makeBin.write((char*)&Depth, sizeof(Depth));
 }
 
 bool BTree::insert(int _key, int _rid) {
-	ofstream insertNode;
-	insertNode.open("btree.bin", ios::out | ios::binary);
-	
+	//ifstream myFile(fileName, ios::in | ios::binary);
+	//int val;
+	//myFile.read((char*)&val, sizeof(int));
 
+	//cout << val << endl;
+
+	FILE* pFile;
+	long iSize;
+	char* buffer;
+	size_t result;
+
+	pFile = fopen(fileName, "rb");
+	fseek(pFile, 0, SEEK_END);
+	iSize = ftell(pFile);
+	rewind(pFile);
+	buffer = (char*)malloc(sizeof(char) * iSize);
+	result = fread(buffer, 1, iSize, pFile);
+	
+	for (int i = 0; i < 12; i++) {
+		cout << (int)buffer[i] << " ";
+	}
+	cout << endl;
+
+	return false;
 }
 
 int* BTree::search(int _key) {
@@ -63,7 +80,9 @@ vector<string> split(string s, string div) {
 
 int main(int argc, char* argv[]) {
 	char command = argv[1][0];
-	BTree* btree;
+	fileName = argv[2];
+
+	BTree* btree = new BTree("", 0);
 	ifstream openText;
 	string buffer;
 
@@ -85,7 +104,6 @@ int main(int argc, char* argv[]) {
 			
 			btree->insert(stoi(input[0]), stoi(input[1]));
 		}
-
 		break;
 	case 's':
 		break;
