a4d63443a68aaaf93af2e14e2664c367e088eccb
dlwjdcks11 <dlwjdcks7356@gmail.com>
2021-06-03 22:59:31 +0900

    Add initialize function

--- BTree.cpp
+++ BTree.cpp
@@ -1,41 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
-
-int blockSize;
-
-class Node {
-public:
-	bool isLeaf;
-	pair<int, int>* keyPair;
-	Node** next;
-	Node();
-};
-
-Node::Node() {
-	this->isLeaf = false;
-	this->keyPair = new pair<int, int>[(blockSize - 4) / 4];
-	this->next = new Node * [(blockSize - 4) / 4 + 1];
-}
 
 class BTree {
 public:
 	const char* fileName;
+	int blockSize;
 
 	BTree(const char*, int);
 	bool insert(int, int);
-	void print();
 	int* search(int);
 	int* search(int, int);
+	void print();
 };
 
 BTree::BTree(const char* _fileName, int _blockSize) {
+	const int RootBID = 1;
+	const int Depth = 0;
 	this->fileName = _fileName;
-	blockSize = _blockSize;
+	this->blockSize = _blockSize;
+
+	ofstream makeBin;
+	makeBin.open(fileName, ios::out | ios::binary);
+	makeBin.write((char*)&blockSize, sizeof(blockSize));
+	makeBin.write((char*)&RootBID, sizeof(RootBID));
+	makeBin.write((char*)&Depth, sizeof(Depth));
 }
 
-bool BTree::insert(int key, int rid) {
+bool BTree::insert(int _key, int _rid) {
+	ofstream insertNode;
+	insertNode.open("btree.bin", ios::out | ios::binary);
+	
+
+}
+
+int* BTree::search(int _key) {
+
+}
+
+int* BTree::search(int _startRange, int _endRange) {
 
 }
 
@@ -43,22 +51,41 @@ void BTree::print() {
 
 }
 
-int* BTree::search(int key) {
-
-}
-
-int* BTree::search(int startRange, int endRange) {
-
+vector<string> split(string s, string div) {
+	vector<string> v;
+	char* c = strtok((char*)s.c_str(), div.c_str());
+	while (c) {
+		v.push_back(c);
+		c = strtok(NULL, div.c_str());
+	}
+	return v;
 }
 
 int main(int argc, char* argv[]) {
 	char command = argv[1][0];
-	BTree* btree = new BTree("", 0);
+	BTree* btree;
+	ifstream openText;
+	string buffer;
+
+	// btree.exe c btree.bin 36 기준으로,
+	// argv[1][0] == c
+	// argv[2] == btree.bin
+	// argv[3] == 36
 
 	switch (command) {
 	case 'c':
+		btree = new BTree(argv[2], atoi(argv[3]));
 		break;
 	case 'i':
+		openText.open(argv[3]);
+
+		while (openText.peek() != EOF) {
+			getline(openText, buffer);
+			vector<string> input = split(buffer, ",");
+			
+			btree->insert(stoi(input[0]), stoi(input[1]));
+		}
+
 		break;
 	case 's':
 		break;
